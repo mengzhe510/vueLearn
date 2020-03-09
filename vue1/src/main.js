7663a018ff1cab1468616d3a0b08f11820a1ecfc
宋孟哲 <mengzhe.song@ly.com>
2020-03-09 13:25:57 +0800

    feat:vuex

--- vue1/src/main.js
+++ vue1/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import './cube-ui'
 import App from './App.vue'
 import router from './router/index'
+import store from './store'
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
